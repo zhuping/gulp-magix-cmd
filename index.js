48e3f73afb92749231f2348be8c122413a127449
zhuping.zp <zhuping.zp@alibaba-inc.com>
2016-08-08 10:20:35 +0800

    升级依赖

--- index.js
+++ index.js
@@ -1,22 +1,13 @@
 var fs = require('fs')
 var through = require('through2')
-
 var combineTool = require('magix-combine');
-
-var wrapTMPL = 'define(\'${moduleId}\',[${requires}],function(require,exports,module){\r\n/*${vars}*/\r\n${content}\r\n});';
-var wrapNoDepsTMPL = 'define(\'${moduleId}\',function(require,exports,module){\r\n${content}\r\n});';
 
 combineTool.config({
   prefix: 'mx',
-  generateJSFile: function(o) {
-    var tmpl = o.requires.length ? wrapTMPL : wrapNoDepsTMPL;
-    for (var p in o) {
-      var reg = new RegExp('\\$\\{' + p + '\\}', 'g');
-      tmpl = tmpl.replace(reg, (o[p] + '').replace(/\$/g, '$$$$'));
-    }
-    return tmpl;
-  }
-});
+  tmplFolder: './',
+  loaderType: 'cmd'
+})
+
 module.exports = function() {
   return through.obj(function(file, enc, cb) {
     combineTool.config({
@@ -27,10 +18,9 @@ module.exports = function() {
       cb(null, file)
     } else {
       combineTool.processContent(file.path, '', jsStr).then(function(content) {
-        //console.log(c);
         file.contents = new Buffer(content);
-        cb(null, file);
-      });
+        cb(null, file)
+      })
     }
   })
 }
